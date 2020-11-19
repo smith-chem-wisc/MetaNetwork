62a724e80b0d3234746a28ad62eb6549cc5b6291
avcarr2 <64652734+avcarr2@users.noreply.github.com>
2020-11-19 15:15:45 -0600

    Update user interface
    Added headings for the sections, added advanced setting, added checkbox for automatic power selection

--- ui.R
+++ ui.R
@@ -13,35 +13,50 @@ options(shiny.maxRequestSize = 30*1024^2) # increases the upload size allowed to
 
 ui <- shinyUI(navbarPage("WGCNA and GO analysis",
                          #Page for step 1 ----
-                         tabPanel("Step 1: WGCNA Analysis", 
-                                  #Side bar controls for step 1    
+                         tabPanel("Step 1: WGCNA Analysis",
+                                  #Side bar controls for step 1
                                   sidebarPanel(
                                     # File input parameters ----
-                                    fileInput(inputId = "dataFile", label = "Data (.csv file) See note for required formatting."), 
-                                    fileInput(inputId = "groupsFile", label = "Groups (.csv file) See note for required formatting."), 
-                                    fileInput(inputId = "databaseFile", label = "Enter Database File Downloaded from Uniprot"),
+                                    h2("File input"),
+                                    fileInput(inputId = "dataFile",
+                                              label = "Data (.csv file) See note for required formatting."),
+                                    fileInput(inputId = "groupsFile",
+                                              label = "Groups (.csv file) See note for required formatting."),
+                                    fileInput(inputId = "databaseFile",
+                                              label = "Enter Database File Downloaded from Uniprot"),
                                     # WGCNA Workflow parameters ----
-                                    textInput(inputId = "rcutoff" , 
-                                              label = "R Cutoff", 
-                                              value = 0.85), 
-                                    textInput(inputId = "mcutheight", 
-                                              label = "M Cut Height", 
-                                              value = 0.25), 
-                                    textInput(inputId = "powerupper", 
-                                              label = "Power Upper", 
-                                              value = 20), 
-                                    textInput(inputId = "minmodsize", 
-                                              label = "Min Module Size", 
-                                              value = 20), 
-                                    textInput(inputId = "ignorecols", 
-                                              label = "Enter how many columns to ignore", 
+                                    h3("WGCNA workflow parameters"),
+                                    textInput(inputId = "rcutoff" ,
+                                              label = "Scale Free Cutoff",
+                                              value = 0.85),
+                                    textInput(inputId = "mcutheight",
+                                              label = "Module Merge Cut Height",
+                                              value = 0.25),
+                                    textInput(inputId = "powerupper",
+                                              label = "Upper Power",
+                                              value = 20),
+                                    textInput(inputId = "minmodsize",
+                                              label = "Minimum Module Size",
+                                              value = 20),
+                                    textInput(inputId = "ignorecols",
+                                              label = "Non-data columns",
                                               value = 2),
+                                    checkboxInput(inputId = "automaticPowerSelection",
+                                                  value = FALSE,
+                                                  label = "Check for automatic power selection"),
+                                    br(),
+                                    h3("Advanced options")
+                                    textInput(inputId = "threads",
+                                              label = "Enter how many threads to use",
+                                              value = ""),
+                                    textInput(inputId = "overridePowerSelection",
+                                              label = "Enter custom power"),
                                     #textInput(inputId = "workbookname", label = "Enter custom name for the excel workbook output", value = ""), Maybe someday.
                                     #checkboxInput(inputId = "ttestbool", label = "Check if data includes column of TTEST values", value = FALSE),
                                     #checkboxInput(inputId = "exportvisAnt", label = "Check if you want to export to VisANT. Not functional yet.", value = FALSE),
                                     #textInput(inputId = "filerename", label = "Enter analysis name", value = ""),
                                     # line break (to make it look nicer) ----
-                                    br(), 
+                                    br(),
                                     # Submit WGCNA Job ----
                                     actionButton(inputId = "action", label = "Submit Job")
                                   ),
@@ -50,21 +65,22 @@ ui <- shinyUI(navbarPage("WGCNA and GO analysis",
                                     ## Need to output the table again. It got accidentally deleted
                                     tableOutput("preview"),
                                     textOutput(outputId = "workflowOutput")
-                                  ) 
-                                  
+                                  )
+
                          ),
                          tabPanel("Step 2: GO Enrichment Analysis",
                                   sidebarPanel(
-                                    fileInput(inputId = "WGCNAResults", label = "Import WGCNA results worksheet", ),
-                                    selectInput(inputId = "organismID", label = "Select organism", choices = c("Human", "Mouse"), 
+                                    fileInput(inputId = "WGCNAResults",
+                                              label = "Import WGCNA results worksheet", ),
+                                    selectInput(inputId = "organismID",
+                                                label = "Select organism",
+                                                choices = c("Human", "Mouse"),
                                                 selected = "Human"),
-                                    br(), 
+                                    br(),
                                     actionButton(inputId = "action2", label = "Submit Job")
-                                  ), 
+                                  ),
                                   mainPanel()
                           )
-                                  
+
   )
 )
-
-
