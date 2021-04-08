b393f1628e06db60c1d1e3e86ac11dff53b6e52a
avcarr2 <64652734+avcarr2@users.noreply.github.com>
2021-04-07 20:25:12 -0500

    Single finy shiny app
    Written in S4 OOP.

--- R/installFunctions.R
+++
@@ -1,8 +0,0 @@
-installFunctions <- function(){
- Rfiles <- list.files(path = "./R")
- RfilesPaths <- paste("R/", Rfiles, sep = "")
- for(i in 1:length(RfilesPaths)){
-   source(RfilesPaths[i])
- }
- message("Successfully loaded endogenous functions")
-}
--- tests/testthat/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(MetaNetwork)
-
-test_check("MetaNetwork")
